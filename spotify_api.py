d192500363372d04a3d37ef7da060c72ef7ef6b4
benproe291 <98323905+benproe291@users.noreply.github.com>
2022-03-31 16:58:18 -0400

    define find_top_artists w/ general API struture

--- spotify_api.py
+++ spotify_api.py
@@ -1,1 +1,19 @@
+import requests
+import pprint
 
+ACCESS_TOKEN = 'BQBZMXhuGFfU9RZC2Y8JfE41i6CZj5dRXh4dwQCjYdx_19uZnBOxrWRSjX5tgZuOQkRqVDzbLfS4C_oYbHroxBHsRjTv-kdXsUX0fT8_PXTKwAdutVc3UmZ29tSzCFnjxflaRDxd3CajGXVNbdqkFcc'
+
+def find_top_songs():
+    response = requests.post(
+        headers = {
+            "Authorization": f'Bearer {ACCESS_TOKEN}'
+        },
+        json = {
+            "type" : "artists",
+            "time_range": "medium_term",
+            "limit": "20"
+        }
+    )
+    json_resp = response.json()
+    print(json_resp)
+    return json_resp
