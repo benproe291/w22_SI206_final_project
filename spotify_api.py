2feb4e3920cdde9d590bbec7691cb446b509f37f
benproe291 <98323905+benproe291@users.noreply.github.com>
2022-03-31 17:04:24 -0400

    def find top songs w/ call error.
    There is an issue with the JSON parsing. I will take more of a look on Friday Apr 1.
    The code seems to be in the correct format, but it is throwing this error "json.decoder.JSONDecodeError: Expecting value: line 1 column 1 (char 0)"

--- spotify_api.py
+++ spotify_api.py
@@ -1,19 +1,23 @@
 import requests
 import pprint
 
-ACCESS_TOKEN = 'BQBZMXhuGFfU9RZC2Y8JfE41i6CZj5dRXh4dwQCjYdx_19uZnBOxrWRSjX5tgZuOQkRqVDzbLfS4C_oYbHroxBHsRjTv-kdXsUX0fT8_PXTKwAdutVc3UmZ29tSzCFnjxflaRDxd3CajGXVNbdqkFcc'
+ACCESS_TOKEN = 'BQAeeZCv5wGKFjjbxLwC44wE4qSjWSIFTdjeS4oIhajyqcI3YUVHjSmbZKzFTfhyZEGdImoRQQsQr9dzI0_-S1ppMZ9IJ484ansqmAV1Gik8xqvBDbnIWnIoHAGv2X20N0xR1_37A0hiJVpfcFO8gLE'
+root_url = "https://api.spotify.com/v1/me/top/"
 
 def find_top_songs():
     response = requests.post(
+        root_url,
         headers = {
             "Authorization": f'Bearer {ACCESS_TOKEN}'
         },
         json = {
             "type" : "artists",
             "time_range": "medium_term",
-            "limit": "20"
+            "limit": "1"
         }
     )
     json_resp = response.json()
     print(json_resp)
-    return json_resp
+
+find_top_songs()
+
