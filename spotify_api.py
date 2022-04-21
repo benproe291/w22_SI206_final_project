1c3d0e5685f182b25b8257ff14afc92744e2c145
benproe291 <98323905+benproe291@users.noreply.github.com>
2022-04-21 16:25:28 -0400

    Spotify API fixed, DB setup

--- spotify_api.py
+++ spotify_api.py
@@ -3,13 +3,16 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import pprint
 
+import sqlite3
+import json
+import os
 
-ACCESS_TOKEN = 'BQAYvAdy7wtzIwjFHOl63mrfdPVEGdOnCZ70P_G3oCRo_IafmmZGR5s77qGup76KXje0IlAgJ5Trw-ipIWGhnjkoPotasMIukg5yJhuqssSsI56hH9MIn2DWgjqaAHLcYqZf06L-RzlEUU09h7S4tKY'
+ACCESS_TOKEN = 'BQCvEyNAX4WwnDf1W5j1SSKQHiTJmH8eHREyH1VOX2cX1rLzJRZxhDqrcUHPO8no4QN1A6zak-dP5SFWkl8u5TOpkju48saQ4C2lT830OMDEQPJKzdImz_KB6kbO8mtC3Fx3k8eK_2ToQDwGQRWl5Ok'
 root_url = "https://api.spotify.com/v1/me/top/artists?"
 
 def find_top_songs():
-    tr = "medium_term"
-    lm = "4"
+    tr = "long_term"
+    lm = "35"
     offset = '0'
     response = requests.get(
         f'https://api.spotify.com/v1/me/top/artists?time_range={tr}&limit={lm}&offset={offset}',
@@ -20,7 +23,39 @@ def find_top_songs():
         }
     )
     jsn = response.json()
-    pprint.pprint(jsn)
+    #pprint.pprint(jsn['items'])
     return jsn
-find_top_songs()
 
+def setUpDatabase(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_name)
+    cur = conn.cursor()
+    return cur, conn
+
+def create_top_artists(cur, conn):
+    cur.execute("DROP TABLE IF EXISTS top_artists")
+    conn.commit()
+    cur.execute('CREATE TABLE top_artists (artist_id INTEGER PRIMARY KEY, name TEXT, genre1 TEXT, genre2 TEXT)')
+    conn.commit()
+
+    data = find_top_songs()
+    pprint.pprint(data['items'])
+    artist_id = 1
+    for item in data['items']:
+        artist_id += 1
+        artist = item['name']
+        try:
+            genre1 = item['genres'][0]
+        except:
+            continue
+        try:
+            genre2 = item['genres'][1]
+        except:
+            genre2 = "null"
+        cur.execute("INSERT INTO top_artists (name, genre1, genre2) VALUES (?,?,?)", (artist, genre1, genre2))
+        print(artist, genre1, genre2)
+    conn.commit()
+
+
+cur, conn = setUpDatabase("artists")
+create_top_artists(cur, conn)
