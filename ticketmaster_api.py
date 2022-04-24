38f1135e18038e22b30335967bb54bd8f3af0bea
Kevin Zhao <kevinmz@Kevins-MacBook-Air.local>
2022-04-24 12:56:22 -0400

    created evets table

--- ticketmaster_api.py
+++ ticketmaster_api.py
@@ -1,20 +1,45 @@
 import requests
+import sqlite3
+import json
+import os
 
-# to get by genre
-genre = "rap"
+def setUpDatabase(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_name)
+    cur = conn.cursor()
+    cur.execute('CREATE TABLE IF NOT EXISTS events (event_id TEXT PRIMARY KEY, event_name TEXT, genre TEXT, date TEXT)')
+    conn.commit()
+    return cur, conn
 
-# to get by artist name
-artist = "21 Savage"
 
-# filter by location (country)
-countryCode = "US"
+def find_events(artist, genre="", countryCode="US"):
+    # filtered data, now data is a list of events
+    url = "https://app.ticketmaster.com/discovery/v2/events.json?keyword={}&classificatioName={}&countryCode={}&apikey=F429VW6ixtsWtGtKWzffWwfzDcO9Ad8x".format(artist, genre, countryCode)
+    data = requests.get(url).json()
+    data = data["_embedded"]["events"]
+    return data
 
 
-# filtered data, now data is a list of events
-url = "https://app.ticketmaster.com/discovery/v2/events.json?keyword={}&classificatioName={}&countryCode={}&apikey=F429VW6ixtsWtGtKWzffWwfzDcO9Ad8x".format(artist, genre, countryCode)
-data = requests.get(url).json()
-data = data["_embedded"]["events"]
-# print(data)
+def create_events(cur, conn, artist, genre="", countryCode="US"):
+
+    # find events
+    events = find_events(artist)
+
+    for event in events:
+        event_id = event["id"]
+        event_name = event["name"]
+        genre = event["classifications"][0]["genre"]["name"]
+        date = event["dates"]["start"]["localDate"]
+        cur.execute("INSERT INTO events (event_id, event_name, genre, date) VALUES (?,?,?,?)", (event_id, event_name, genre, date))
+
+    conn.commit()
+
+
+cur, conn = setUpDatabase("top_artists_concerts")
+artist = "Dababy"
+create_events(cur, conn, artist)
+
+
 
 ### -- MAKE SURE TO HIT -- # (300 points)
 # Get at least 100 rows (have multiple calls to multiple favorite artists)
